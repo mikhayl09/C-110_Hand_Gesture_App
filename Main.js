79bbc59460b25ee38bb33de7233dbd09030333c1
mikhayl09 <82265508+mikhayl09@users.noreply.github.com>
2023-05-19 14:50:09 +0530

    Add files via upload

--- Main.js
+++ Main.js
@@ -1,5 +1,5 @@
-prediction_1 = " "
-prediction_2 = " "
+prediction_1 = " ";
+prediction_2 = " ";
 
 Webcam.set({
     width:350,
@@ -29,4 +29,40 @@ function speak(){
     speak_data_2 = "And the second prediction is" + prediction_2;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if (error){
+    console.error(error);
+} else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "Thumbs Up") {
+        document.getElementById("update_emoji").innerHTML = "&#128077";
+        }
+        if(results[0].label == "Victory") {
+            document.getElementById("update_emoji").innerHTML = "&#9996";
+            }
+            if(results[0].label == "OK") {
+                document.getElementById("update_emoji").innerHTML = "&#128076";
+                }
+                if(results[1].label == "Thumbs Up") {
+                    document.getElementById("update_emoji2").innerHTML = "&#128077";
+                    }
+                    if(results[1].label == "Victory") {
+                        document.getElementById("update_emoji2").innerHTML = "&#9996";
+                        }
+                        if(results[1].label == "OK") {
+                            document.getElementById("update_emoji2").innerHTML = "&#128076";
+                            }
+}
 }
